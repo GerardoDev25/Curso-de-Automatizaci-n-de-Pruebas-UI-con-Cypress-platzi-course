9895b4176f43a82905ad28f7cb62b9d1765fe3dc
Gerardo Miranda <gerardodev25@gmail.com>
2022-10-13 23:20:05 -0400

    Nuestra primer prueba

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    excludeSpecPattern: [
+      'cypress/e2e/1-getting-started/*.js',
+      'cypress/e2e/2-advanced-examples/*.js',
+    ],
   },
 });
