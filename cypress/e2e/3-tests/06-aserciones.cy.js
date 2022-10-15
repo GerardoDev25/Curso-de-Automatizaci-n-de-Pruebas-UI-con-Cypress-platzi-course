9f805f41575648f5b9e39c2ebe66e7c898b8ecd9
Gerardo Miranda <gerardodev25@gmail.com>
2022-10-14 23:57:53 -0400

    Hooks

--- cypress/e2e/3-tests/06-aserciones.cy.js
+++ cypress/e2e/3-tests/06-aserciones.cy.js
@@ -3,15 +3,30 @@ describe('aserciones', () => {
     return false;
   });
 
-  it('Asercion', () => {
+  before(() => {
     cy.visit('/automation-practice-form');
+  });
+
+  after(() => {
+    cy.visit('/');
+  });
+
+  // afterEach(() => {
+  //   cy.visit('/');
+  // });
+
+  // beforeEach(() => {
+  //   cy.visit('/automation-practice-form');
+  // });
+
+  it('Asercion', () => {
     cy.url().should('include', 'demoqa.com');
     // cy.get('#firstName').should('be.visible').should('have.attr', 'placeholder', 'First Name');
     cy.get('#firstName').should('be.visible').and('have.attr', 'placeholder', 'First Name');
   });
 
   it('Asercion 2', () => {
-    cy.visit('/automation-practice-form');
+    // cy.visit('/automation-practice-form');
     cy.url().should('include', 'demoqa.com');
     cy.get('#firstName').then((element) => {
       expect(element).to.be.visible;
@@ -20,7 +35,7 @@ describe('aserciones', () => {
   });
 
   it('Asercion 3', () => {
-    cy.visit('/automation-practice-form');
+    // cy.visit('/automation-practice-form');
     cy.url().should('include', 'demoqa.com');
     cy.get('#firstName').then((element) => {
       assert.equal(element.attr('placeholder'), 'First Name');
