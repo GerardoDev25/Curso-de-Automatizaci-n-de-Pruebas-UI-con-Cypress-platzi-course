4718522fe3a21ecc8080aa38f324e3151e9641a3
Gerardo Miranda <gerardodev25@gmail.com>
2022-10-18 23:50:08 -0400

    Trabajando con inputs

--- cypress/e2e/3-tests/08-interactuando-con-los-objetos.cy.js
+++ cypress/e2e/3-tests/08-interactuando-con-los-objetos.cy.js
@@ -2,7 +2,7 @@ describe('interactuando con los objecots', () => {
   Cypress.on('uncaught:exception', (err, runnable) => {
     return false;
   });
-  it('click', () => {
+  it.skip('click', () => {
     cy.visit('/buttons');
     cy.get('button').eq(3).click();
     cy.get('#dynamicClickMessage')
@@ -10,7 +10,7 @@ describe('interactuando con los objecots', () => {
       .and('contain', 'You have done a dynamic click');
   });
 
-  it('double click', () => {
+  it.skip('double click', () => {
     cy.visit('/buttons');
     cy.get('#doubleClickBtn').dblclick();
     cy.get('#doubleClickMessage')
@@ -18,21 +18,50 @@ describe('interactuando con los objecots', () => {
       .and('contain', 'You have done a double click');
   });
 
-  it('right click', () => {
+  it.skip('right click', () => {
     cy.visit('/buttons');
     cy.get('#rightClickBtn').rightclick();
     cy.get('#rightClickMessage').should('be.visible').and('contain', 'You have done a right click');
   });
 
-  it('force click', () => {
+  it.skip('force click', () => {
     cy.visit('/dynamic-properties');
     cy.get('#enableAfter').click({ timeout: 0, force: true });
   });
 
-  it.only('click por posicion', () => {
+  it.skip('click por posicion', () => {
     cy.visit('/buttons');
     cy.get('button').eq(3).parent().parent().click('topRight');
     cy.get('button').eq(3).parent().parent().click('bottomLeft');
     cy.get('button').eq(3).parent().parent().click(5, 6);
   });
+
+  it('input type text', () => {
+    cy.visit('/automation-practice-form');
+    cy.get('#firstName').type('Gerardo');
+    cy.get('#lastName').type('Miranda');
+
+    cy.get('#firstName').type('{selectAll}{backspace}');
+    cy.get('#firstName').type('another name');
+    cy.get('#lastName').clear();
+    cy.get('#lastName').type('another last name');
+  });
+
+  // ? commons list for type
+  // {backspace} Borra el personaje a la izquierda del cursor.
+  // {del} Borra el personaje a la derecha del cursor.
+  // {downarrow} Mueve el cursor hacia abajo.
+  // {end}	Mueve el cursor al final de la línea.
+  // {enter} Teclea la tecla Intro.
+  // {esc}	Teclea la tecla Escape.
+  // {home} Mueve el cursor al principio de la línea.
+  // {insert} Inserta un personaje a la derecha del cursor.
+  // {leftarrow} Mueve el cursor a la izquierda.
+  // {movetoend} Desplaza el cursor al final del elemento mecanizable.
+  // {movetostart} Desplaza el cursor al inicio del elemento mecanizable.
+  // {pagedown} Se desplaza hacia abajo.
+  // {pageup}  Se desplaza hacia arriba.
+  // {rightarrow} Mueve el cursor a la derecha.
+  // {selectall} Selecciona todo el texto creando un selection range.
+  // {uparrow}	Mueve el cursor hacia arriba.
 });
